cea7c48f538949955d6dc672e06756b724c3e91b
Elina <elina.zekunde@gmail.com>
2021-02-02 14:00:45 +0200

    updated finder

--- DayFinder/ViewController.swift
+++ DayFinder/ViewController.swift
@@ -23,12 +23,70 @@ class ViewController: UIViewController {
     }
 
     @IBAction func findWeekdayTapped(_ sender: Any) {
-        //calendar
-        //DateComponents()
-        // dateComponents.day = dayTextField.text
-        //calendar.date(from: dateComponents)
-        //DateFormatter()
-        //resultLabel.text = result
+        let calendar = Calendar.current
+        let formatter = DateFormatter()
+        
+        var day = ""
+        var month = ""
+        var year = ""
+        
+        guard let dayValue = dayTextField.text else { return }
+        day = dayValue
+
+        guard let monthValue = monthTextField.text else { return }
+        month = monthValue
+
+        guard let yearValue = yearTextField.text else { return }
+        year = yearValue
+        
+//        if let value = dayTextField.text {
+//            day = value
+//        } else {
+//            return
+//        }
+//
+//        if let value = monthTextField.text {
+//            month = value
+//        } else {
+//            return
+//        }
+//
+//        if let value = yearTextField.text {
+//            year = value
+//        } else {
+//            return
+//        }
+        
+        let dateString = "\(day).\(month).\(year)"
+        
+        formatter.dateFormat = "dd.MM.yyyy"
+        
+        let date = formatter.date(from: dateString)
+        
+        let result = calendar.dateComponents([.weekday], from: date!).weekday
+        
+        var dayOfTheWeek = ""
+        
+        switch result {
+        case 1:
+            dayOfTheWeek = "Sunday"
+        case 2:
+            dayOfTheWeek = "Monday"
+        case 3:
+            dayOfTheWeek = "Tuesday"
+        case 4:
+            dayOfTheWeek = "Wednesday"
+        case 5:
+            dayOfTheWeek = "Thursday"
+        case 6:
+            dayOfTheWeek = "Friday"
+        case 7:
+            dayOfTheWeek = "Saturday"
+        default:
+            dayOfTheWeek = "unknown"
+        }
+        
+        resultLabel.text = dayOfTheWeek
     }
     
 }
